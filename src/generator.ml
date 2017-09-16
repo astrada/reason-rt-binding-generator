b789f4c2250d37141561562da79e48a433ee049e
Alessandro Strada <alessandro.strada@gmail.com>
2017-09-16 16:53:10 +0200

    Rename files to uppercase

--- src/generator.ml
+++ src/generator.ml
@@ -3,16 +3,16 @@ let () =
       "../../../output/react-toolbox.typedoc.json" in
 
   React_toolbox_writer.write_re ~bundled:false
-    "../../../output/reactToolbox.re"
+    "../../../output/ReactToolbox.re"
     component_list;
   React_toolbox_writer.write_rei ~bundled:false
-    "../../../output/reactToolbox.rei"
+    "../../../output/ReactToolbox.rei"
     component_list;
 
   React_toolbox_writer.write_re ~bundled:true
-    "../../../output/reactToolboxBundled.re"
+    "../../../output/ReactToolboxBundled.re"
     component_list;
   React_toolbox_writer.write_rei ~bundled:true
-    "../../../output/reactToolboxBundled.rei"
+    "../../../output/ReactToolboxBundled.rei"
     component_list
 
